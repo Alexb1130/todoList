2f052a0e9b2c8be8a138cf109d9282a894c2538f
alexb <b4zx720@gmail.com>
2019-01-29 09:54:12 +0300

    uptade service

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 (function() {
+    var service = new Service();
     var model = new Model([]);
+    service.save(model);
     var view = new View(model);
+    var controller = new Controller(model, view, service);
     view.input.focus();
-    var controller = new Controller(model, view);
 })()
--- js/service.js
+++ js/service.js
@@ -3,7 +3,7 @@ function Service(data) {
 };
 Service.prototype.save = function(data) {
     var string = JSON.stringify(data);
-    localStorage.setItem('tasks', string);   
+    localStorage.setItem('tasks', string);
 };
 Service.prototype.load = function() {
     var string = localStorage.getItem('tasks')
@@ -11,3 +11,52 @@ Service.prototype.load = function() {
 
     return data;
 };
+Service.prototype.addItem = function(model, view) {
+    view.form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if(view.input.value) {
+            var task = model.addItem({
+                id: Date.now(),
+                title: view.input.value,
+                completed: false
+            });
+            view.addItem(task);
+        } else {
+            alert('Поле должно быть заполненно');
+        }
+    });
+}
+Service.prototype.addEventListeners = function(model, view) {
+    document.addEventListener('click', function(e) {
+
+        var isRemove = e.target.classList.contains('btn-remove');
+        var isCompleted = e.target.classList.contains('my-checkbox');
+        var isEditing = e.target.classList.contains('btn-edit');
+
+        if(isRemove) {
+            var taskId = e.target.parentNode.getAttribute('data-id');
+            view.hendleRemove(e);
+            model.removeItem(taskId);
+            console.log(model);
+
+        } else if(isCompleted) {
+            var checkbox = e.target;
+            var taskId = e.target.parentNode.parentNode.getAttribute('data-id');
+            view.hendleCompleted(e);
+
+            if(checkbox.checked) {
+                model.updateItem(taskId, { completed: true });
+                console.log(model, taskId);
+            } else {
+                model.updateItem(taskId, { completed: false }); 
+                console.log(model, taskId);
+            }
+        } else if(isEditing) {
+            var taskId = e.target.parentNode.getAttribute('data-id');
+            view.hendleEditing(e);
+            var value = e.target.parentNode.querySelector('.task-content').textContent;
+            model.updateItem(taskId, { title: value });
+            console.log(model, taskId);
+        }
+    })
+}
